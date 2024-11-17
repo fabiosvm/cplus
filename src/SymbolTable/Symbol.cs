9c039ab5863578d44814d120c97dfcce8e38d79c
fabiosvm <fabiosvm@outlook.com>
2024-11-16 23:01:51 -0300

    Add Symbol Table

--- src/SymbolTable/Symbol.cs
+++ src/SymbolTable/Symbol.cs
@@ -1,5 +1,21 @@
 
 public class Symbol
 {
-  // TODO: Implement the Symbol class.
+  public Token Ident { get; }
+  public string Name { get; }
+  public SymbolKind Kind { get; }
+  public int Depth { get; }
+  public int Index { get; }
+  public int Used { get; set; } = 0;
+
+  public bool IsLocal => Depth > 0;
+
+  public Symbol(Token ident, SymbolKind kind, int depth, int index)
+  {
+    Ident = ident;
+    Name = ident.Lexeme;
+    Kind = kind;
+    Depth = depth;
+    Index = index;
+  }
 }
--- src/SymbolTable/SymbolTable.cs
+++ src/SymbolTable/SymbolTable.cs
@@ -1,5 +1,43 @@
 
 public class SymbolTable
 {
-  // TODO: Implement the SymbolTable class.
+  public Dictionary<string, Symbol> Symbols = new Dictionary<string, Symbol>();
+  private int scopeDepth { get; set; } = 0;
+  private int nextIndex = 0;
+
+  public void EnterScope()
+  {
+    scopeDepth++;
+  }
+
+  public void ExitScope()
+  {
+    scopeDepth--;
+  }
+
+  public Symbol? Define(Token ident, SymbolKind kind)
+  {
+    var name = ident.Lexeme;
+
+    if (Symbols.ContainsKey(name))
+      return null;
+
+    var symbol = new Symbol(ident, kind, scopeDepth, nextIndex);
+    Symbols[name] = symbol;
+    nextIndex++;
+
+    return symbol;
+  }
+
+  public Symbol? Resolve(Token ident)
+  {
+    var name = ident.Lexeme;
+    Symbol? symbol;
+
+    if (!Symbols.TryGetValue(name, out symbol))
+      return null;
+
+    symbol.Used++;
+    return symbol;
+  }
 }
